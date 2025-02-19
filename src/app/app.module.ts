72f0c7075c19069d4afd14323f8832b7426f27d5
panchanan-turuk <pturuk123@gmail.com>
2025-02-19 13:07:37 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './header/top-header/top-header.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopHeaderComponent,
+    MainMenuComponent,
+    ProductListComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule
